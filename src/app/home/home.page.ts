76a814428061c572716912c035e9ab7100e7d395
Aluno <you@example.com>
2019-07-03 20:34:22 -0300

    metodo delete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,9 +9,20 @@ export class HomePage {
 
   constructor() { }
 
-  contato = 
+  contatos = [
     {
       "nome": "bruno",
       "url": "https://i.pinimg.com/originals/de/f6/96/def69643889ee29e232637646e839064.jpg"
-    }   
+    },
+    {
+      "nome": "muller",
+      "url": "http://www.aranhahomem.com/images/pictures/fantastico-homem-aranha.jpg"
+    }
+  ];
+
+  delete(contato){
+    var i = this.contatos.indexOf(contato);
+    this.contatos.splice(i, 1);
+  }
+
 }
